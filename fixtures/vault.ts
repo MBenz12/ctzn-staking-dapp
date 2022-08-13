626ac6a80ede877e179ea56d29e533da54f24cfb
mbenz <mbenz10120112@gmail.com>
2022-08-12 22:36:24 -0400

    add await

--- fixtures/vault.ts
+++ fixtures/vault.ts
@@ -283,7 +283,7 @@ export class Vault {
     nft: PublicKey,
   ) {
     const mint = new Mint(nft, null, this.program, 0);
-    const stakeAccount = mint.getAssociatedTokenAddress(curAuthoriy.publicKey);
+    const stakeAccount = await mint.getAssociatedTokenAddress(curAuthoriy.publicKey);
     console.log(stakeAccount.toString());
     let txSignature;
     let tx = await this.program.transaction.stake(itemType, {
