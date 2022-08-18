7af3ec3b74071e09d6d4d250f81fba86f9f5c88e
mbenz <mbenz10120112@gmail.com>
2022-08-17 23:06:12 -0400

    increase index

--- pages/api/admin.js
+++ pages/api/admin.js
@@ -2,7 +2,7 @@ import { writeFile } from "fs";
 import { Connection, clusterApiUrl, Keypair, PublicKey } from "@solana/web3.js";
 import { Metaplex, keypairIdentity } from "@metaplex-foundation/js";
 import mints from '../../fixtures/777ctzns-hashlist.json';
-
+import { sleep } from "../../fixtures/lib";
 
 export default async function handler(req, res) {
     const connection = new Connection(clusterApiUrl("mainnet-beta"));
@@ -17,7 +17,9 @@ export default async function handler(req, res) {
         writeFile(`metadata/${i}.json`, JSON.stringify(nft.metadata), (err) => {
             if (err) throw err;
             console.log("success");
-        })
+        });
+        i++;
+        await sleep(1000);
     }
 
     res.status(200).json({ name: 'John Doe' })
